72929d716aad8be9adcae18d663362f61ca3fb99
Vimukthi_Oshada <108446553+oshadaXD@users.noreply.github.com>
2022-07-14 17:16:48 +0530

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: node . -- db 'mongodb+srv://oshada:oshada@oshada.axmu8.mongodb.net/?retryWrites=true&w=majority'
---
+++ config.js
@@ -0,0 +1,52 @@
+/**
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/6288292024190
+   * Follow https://github.com/DikaArdnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://api.zeeoneofc.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://api.zeeoneofc.xyz': 'XGwvPEbx',
+}
+
+// Other
+global.owner = ['94775792013']
+global.premium = ['94775792013']
+global.packname = '⛧ S A T H A N ⸸'
+global.author = '⛧ S A T H A N ⸸'
+global.sessionName = 'hisoka'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./lib/hisoka.jpg')
+global.visoka = { url: 'https://telegra.ph/file/39c0aba89c1281605b7de.mp4' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
